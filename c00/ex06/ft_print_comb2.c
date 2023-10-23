37b5940ca9c5a6fad30ade7611cc2718cc5b34a9
Bero000 <122268572+Bero000@users.noreply.github.com>
2023-10-23 23:50:59 +0300

    c00

---
+++ c00/ex06/ft_print_comb2.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: beeren <beeren@student.42kocaeli.com.tr>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/04 22:02:28 by beeren            #+#    #+#             */
+/*   Updated: 2023/02/04 22:35:57 by beeren           ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char a)
+{
+	write(1, &a, 1);
+}
+
+void	ft_print_comb2(void)
+{
+	int	a;
+	int	b;
+
+	a = -1;
+	while (a++ < 98)
+	{
+		b = a;
+		while (b++ < 99)
+		{
+			ft_putchar((a / 10) + 48);
+			ft_putchar((a % 10) + 48);
+			ft_putchar(' ');
+			ft_putchar((b / 10) + 48);
+			ft_putchar((b % 10) + 48);
+			if (a != 98)
+			{
+				ft_putchar(',');
+				ft_putchar(' ');
+			}
+		}
+	}
+}
---
+++ c00/ex07/ft_putnbr.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: beeren <beeren@student.42kocaeli.com.tr>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/04 22:13:34 by beeren            #+#    #+#             */
+/*   Updated: 2023/02/04 22:38:09 by beeren           ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{	
+	if (nb == -2147483648)
+	{
+		ft_putchar('-');
+		ft_putchar('2');
+		write(1, "147483648", 9);
+	}
+	else if (nb < 0)
+	{
+		ft_putchar('-');
+		ft_putnbr(-nb);
+	}
+	else if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else if (nb <= 9)
+	{
+		ft_putchar(nb + 48);
+	}
+}
