6ac5752e38db4fbe0af3a36a9384bd15bc41dc2e
Bero000 <122268572+Bero000@users.noreply.github.com>
2023-10-23 23:50:21 +0300

    rush00

---
+++ rush00/ex00/rush01.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush01.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: naiskend <naiskend@student.42kocaeli.com.  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/05 18:45:50 by naiskend          #+#    #+#             */
+/*   Updated: 2023/02/05 20:25:52 by naiskend         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+void	ft_print(int count, char ilk_char, char orta_char, char son_char)
+{
+	int	i;
+
+	i = 1;
+	while (i <= count)
+	{
+		if (i == 1)
+			ft_putchar(ilk_char);
+		else if (i == count)
+			ft_putchar(son_char);
+		else
+			ft_putchar(orta_char);
+		i++;
+	}
+	ft_putchar('\n');
+}
+
+void	rush(int x, int y)
+{
+	int	i;
+
+	i = 1;
+	if (y < 0 || x < 0)
+		write(1, "BEN BUNU YAPAMAM", 16);
+	if (x >= 1 && y >= 1)
+	{
+		while (i <= y)
+		{
+			if (i == 1)
+				ft_print(x, '/', '*', '\\');
+			else if (i == y)
+				ft_print(x, '\\', '*', '/');
+			else
+				ft_print(x, '*', ' ', '*');
+			i++;
+		}
+	}
+}
---
+++ rush00/ex00/rush04.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush04.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: naiskend <naiskend@student.42kocaeli.com.  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/05 00:04:56 by naiskend          #+#    #+#             */
+/*   Updated: 2023/02/05 18:44:39 by naiskend         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+void	ft_print(int count, char ilk_char, char orta_char, char son_char)
+{
+	int	i;
+
+	i = 1;
+	while (i <= count)
+	{
+		if (i == 1)
+			ft_putchar(ilk_char);
+		else if (i == count)
+			ft_putchar(son_char);
+		else
+			ft_putchar(orta_char);
+		i++;
+	}
+	ft_putchar('\n');
+}
+
+void	rush(int x, int y)
+{
+	int	i;
+
+	i = 1;
+	if (y < 0 || x < 0)
+		write(1, "BEN BUNU YAPAMAM", 16);
+	if (x >= 1 && y >= 1)
+	{
+		while (i <= y)
+		{
+			if (i == 1)
+				ft_print(x, 'A', 'B', 'C');
+			else if (i == y)
+				ft_print(x, 'C', 'B', 'A');
+			else
+				ft_print(x, 'B', ' ', 'B');
+			i++;
+		}
+	}
+}	
